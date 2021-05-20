0fc0f595908dbc2a44a65edbee66b41fbb498ae8
Sophie Li <limisoph@gmail.com>
2021-05-20 15:43:47 -0400

    comment out save

--- scripts/fit_model.py
+++ scripts/fit_model.py
@@ -87,7 +87,7 @@ def main():
     df = read_data("immediate_test_clean.csv")
     fitted_params = minimize(sse, initial_guess, tol=1e-3, method="Powell")
     print(fitted_params)
-    np.savetxt("fitted_params.txt", fitted_params["x"], fmt="%s")
+    # np.savetxt("fitted_params.txt", fitted_params["x"], fmt="%s")
 
 
 if __name__ == "__main__":
